8d72933e23b9f1ceaeeea9a79c5f975a1a28b997
Tanmoy Choudhury <official.tanmoychoudhury@protonmail.com>
2023-11-27 23:49:08 +0530

    Navbar and Carousal structure designed

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ import {
 } from "@tanstack/react-query";
 import "./App.css";
 import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Plans from "./pages/Plans";
+import AboutUs from "./pages/AboutUs";
 import Parent from "./pages/Parent";
 import Child from "./pages/Child";
-import Blogs from "./pages/Blogs";
 import Videos from "./pages/Videos";
+import Navbar from "./components/Navbar";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -21,11 +24,14 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        <Navbar />
         <Routes>
           <Route path="/" Component={Home} />
+          <Route path="/blogs" Component={Blogs} />
+          <Route path="/plans" Component={Plans} />
+          <Route path="/about-us" Component={AboutUs} />
           <Route path="/parent" Component={Parent} />
           <Route path="/child" Component={Child} />
-          <Route path="/blogs" Component={Blogs} />
           <Route path="/videos" Component={Videos} />
         </Routes>
       </QueryClientProvider>
---
+++ src/pages/Plans.jsx
@@ -0,0 +1,9 @@
+const Plans = () => {
+  return (
+    <>
+      <h1>Plans</h1>
+    </>
+  );
+};
+
+export default Plans;
